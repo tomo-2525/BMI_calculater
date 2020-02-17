b55df7a639296e39082a0cd5fb37dec51127bab7
tomo-2525 <tomoblog.0205@gmail.com>
2020-02-18 03:36:56 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,37 +1,49 @@
-(function(){
+(function () {
+  //ブラウザがエラーチェックを行う
   'use strict';
 
-var btn = document.getElementById('btn');
-var do1 = document.getElementById('do1');
-var do2 = document.getElementById('do2');
-var result = document.getElementById('result');
-var reset = document.getElementById('reset');
+  var btn = document.getElementById('btn');
+  var weight = document.getElementById('weight');
+  var height = document.getElementById('height');
+  var result = document.getElementById('result');
+  var reset = document.getElementById('reset');
 
-//ボタンを押したときのアニメーション
-btn.addEventListener('mousedown',function(){
-  this.className ='pushed';
-});
+  //ボタンを押したときのアニメーション
+  btn.addEventListener('mousedown', function () {
+    this.className = 'pushed';
+  });
 
-//ボタンから離れたときの処理
-btn.addEventListener('mouseup',function(){
-  this.className ='';
-});
+  //ボタンから離れたときの処理
+  btn.addEventListener('mouseup', function () {
+    this.className = '';
+  });
 
-//ボタンをクリックしたときの処理
-btn.addEventListener("click",function(){
-  var choise = [do1,do2];
-  var n = Math.floor(Math.random()*choise.length);
-result.textContent  = `今日は「${choise[n].value}」をしよう！`;
-reset.classList.remove("hidden");
-});
+  //ボタンをクリックしたときの処理
+  //ここでBMIの計算を行う
+  btn.addEventListener("click", function () {
+    var wei = Number(weight);
+    var hei = Number(height);
 
-reset.addEventListener("click",function(){
- result.textContent = "結果はここに表示されます";
- do1.value ="";
- do2.value = "";
- do1.focus();
-});
+    var bmi = wei / (hei * hei);
+    var proper_wei = (hei * hei) * 22;
+
+    result.textContent = String(bmi);
+
+    /*var choise = [do1, do2];
+    var n = Math.floor(Math.random() * choise.length);
+    result.textContent = `今日は「${choise[n].value}」をしよう！`;
+    reset.classList.remove("hidden");
+    */
+  });
 
 
-do1.focus();
+  reset.addEventListener("click", function () {
+    result.textContent = "結果はここに表示されます";
+    weight.value = "";
+    height.value = "";
+    weight.focus();
+  });
+
+
+  weight.focus();
 })();
